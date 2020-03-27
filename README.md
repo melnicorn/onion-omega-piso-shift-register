f7e7076fe7a688842023f6040efd04b84c23c579
Chris Melnick <cmelnick@gmail.com>
2020-03-27 12:14:18 -0500

    adding readme, example

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# onion-omega-piso-shift-register
-Driver for 74HC165 Parallel-In/Serial-Out shift register for the Onion Omega platform
+
+# Onion Omega Parallel-In/Serial-Out Shift Register Driver
+
+A driver to allow the Onion Omega platform to communicate with a PISO shift register in the 74HC165 chipset. This was developed using the Fairchild MM74HC165 chip so make sure to refer to the data sheet for the specific chip you are working with.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+ - Onion Omega hobby computer with...
+	 - Omega Python light installed
+	 - Omega GPIO libraries installed
+```
+# Run from the Onion Omega
+> opkg update
+> opkg install python-light pyOnionGpio
+```
+- Wire your Omega up to the PISO shift register. This uses four GPIO pins on the Omega.
+![Fritzing diagram of Omega wired to a single PISO shift register](omega_74HC165.png)
+
+**Note** To wire up more than one PISO shift register, connect the same Load/Shift line, Clock Inhibit, and Clock wires to the second shift register. Then connect the Data (Qh) line from the second shift register to the Serial In line of the first register.
+
+### Installing
+For now, just grab [pisoDriver.py](pisoDriver.py) and go to town!
+
+## Authors
+
+* **Chris Melnick** - *Initial work* - [melnicorn](https://github.com/melnicorn)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* [Arduino Playground](https://playground.arduino.cc/Code/ShiftRegSN74HC165N/) PISO tutorial and C code
---
+++ example.py
@@ -0,0 +1,67 @@
+import pisoDriver
+import onionGpio
+import time
+from signal import signal, SIGINT
+
+BUTTON_1 = 0x0001
+BUTTON_2 = 0x0002
+BUTTON_3 = 0x0004
+BUTTON_4 = 0x0008
+BUTTON_5 = 0x0010
+BUTTON_6 = 0x0020
+BUTTON_7 = 0x0030
+BUTTON_8 = 0x0040
+
+class TestPISO:
+    def __init__(self):
+        self.__piso = pisoDriver.PISO(0, 1, 2, 3)
+        self.__running = False
+
+    def loop(self):
+        oldValue = 0x0
+        self.__running = True
+        while self.__running:
+            value = self.__piso.readRegister()
+            if oldValue != value and value > 0x0:
+                self.__print(value)
+            oldValue = value
+            time.sleep(0.001)
+        
+        exit(0)
+
+    def __print(self, value):
+        if value & BUTTON_1 == BUTTON_1:
+            print('Button 1')
+        if value & BUTTON_2 == BUTTON_2:
+            print('Button 2')
+        if value & BUTTON_3 == BUTTON_3:
+            print('Button 3')
+        if value & BUTTON_4 == BUTTON_4:
+            print('Button 4')
+        if value & BUTTON_5 == BUTTON_5:
+            print ('Button 5')
+        if value & BUTTON_6 == BUTTON_6:
+            print ('Button 6')
+        if value & BUTTON_7 == BUTTON_7:
+            print ('Button 7')
+        if value & BUTTON_8 == BUTTON_8:
+            print ('Button 8')
+
+    def kill(self):
+        self.__running = False
+
+instance = None
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    instance.kill()
+
+if __name__ == '__main__':
+    # Tell Python to run the handler() function when SIGINT is recieved
+    signal(SIGINT, handler)
+
+    print('Running. Press CTRL-C to exit.')
+   
+    instance = TestPISO()
+    instance.loop()
